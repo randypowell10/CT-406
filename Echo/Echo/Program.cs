1c32df403304bbbbf84137a061c581a94c5e006a
randypowell10 <randypowell10@yahoo.com>
2018-09-19 18:48:58 -0400

    echo

---
+++ Echo/Echo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Echo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseIISIntegration()
+                .Configure(app =>
+                {
+                    app.Run(async (context) =>
+                    {
+                        var path = context.Request.Path;
+                        await context.Response.WriteAsync(path);
+                    });
+                })
+                .Build();
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Echo/README.md
@@ -0,0 +1,6 @@
+Randy Powell
+CT 406
+Capitol Technology University
+Chapter 2 example: Echo
+
+This is the finished project of the Echo demo in chapter 2 of the textbook.
