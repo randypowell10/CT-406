be2cde3c6cd6cbee75e8aff9507fded5f4325d3b
randypowell10 <randypowell10@yahoo.com>
2018-09-26 15:54:46 -0400

    pub

---
+++ MvcMovie/MvcMovie/obj/Release/netcoreapp2.1/PubTmp/EFSQLScripts/MvcMovie.Models.MvcMovieContext.sql
@@ -0,0 +1,48 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180925180626_Initial')
+BEGIN
+    CREATE TABLE [Movie] (
+        [ID] int NOT NULL IDENTITY,
+        [Title] nvarchar(max) NULL,
+        [ReleaseDate] datetime2 NOT NULL,
+        [Genre] nvarchar(max) NULL,
+        [Price] decimal(18, 2) NOT NULL,
+        CONSTRAINT [PK_Movie] PRIMARY KEY ([ID])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180925180626_Initial')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20180925180626_Initial', N'2.1.2-rtm-30932');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180925192620_Rating')
+BEGIN
+    ALTER TABLE [Movie] ADD [Rating] nvarchar(max) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180925192620_Rating')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20180925192620_Rating', N'2.1.2-rtm-30932');
+END;
+
+GO
+
